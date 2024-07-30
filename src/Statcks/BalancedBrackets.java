258c6d49bcad2c20cc06a2065f3a530d8a737bbb
rokielal <manoranjangupta94@gmail.com>
2024-07-30 10:26:13 +0530

    Stack valid paranthesis problem done

---
+++ src/Statcks/BalancedBrackets.java
@@ -0,0 +1,46 @@
+package Statcks;
+
+import java.util.Stack;
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']
+// ', determine if the input string is valid.
+public class BalancedBrackets {
+
+    public static boolean isBalanced (String s){
+
+        // Create an empty stack to keep track of opening brackets
+        Stack<Character> stack = new Stack<Character>();
+
+        // Loop through every character in the string
+        for (char c : s.toCharArray()) {
+            // If the character is an opening bracket, push it onto the stack
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            else { // If the character is a closing bracket
+                // If the stack is empty, there is no matching opening bracket, so return false
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                // Otherwise, get the top of the stack and check if it's the matching opening bracket
+                char top = stack.peek();
+                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+                    // If it is, pop the opening bracket from the stack
+                    stack.pop();
+                } else { // Otherwise, the brackets don't match, so return false
+                    return false;
+                }
+            }
+        }
+        // If the stack is empty, all opening brackets have been closed, so return true
+        // Otherwise, there are unmatched opening brackets, so return false
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+
+        String str = "()]{}";
+
+        boolean ans = isBalanced( str);
+        System.out.println(ans);
+    }
+}
